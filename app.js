baa9d46bbfcf5dd7d7a9b04af20f740503613d82
Orkun Ozekin <orkun@cloverly.com>
2022-06-02 07:46:08 -0500

    Implemented add functionality

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
 //SELECTORS
-document.querySelector('form').addEventListener('submit',handleSubmitForm);
-document.querySelector('ul').addEventListener('click',handleClickDeleteOrCheck)
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck)
 
 //EVENT HANDLER
-function handleSubmitForm(e){
+function handleSubmitForm(e) {
     e.preventDefault(); //prevent default action of submit
-    let input = document.querySelector('input');
-
-    if (input.value != ' ');
-        addTodo (input.value)
-    input.value = ' ';
-
+    let taskInput = document.getElementById('task-input')
+    console.log(taskInput.value)
+    if (taskInput.value != ' ') {
+        addTodo(taskInput.value)
+    }
+    taskInput.value = ' '
 }
 
-function handleClickDeleteOrCheck(e){
-    if(e.target.name == 'checkButton')
-        checkTodo(e);
+function handleClickDeleteOrCheck(e) {
+    if (e.target.name == 'checkButton')
+        checkTodo(e)
 
-    if(e.target.name == 'deleteButton')
-        checkTodo(e);
-
+    if (e.target.name == 'deleteButton')
+        checkTodo(e)
 }
 
 //HELPER
-function addTodo(todo){
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
-
-    li.innerHTML = ` 
-
-        <span class ="todo-items>${todo}</span>
-        <button name ="checkButton"><i class = "fas fa-check-square"></i></button>
-        <button name = "deleteButton"><i class ="fas fa-trash"></i></button>
+function addTodo(todo) {
+    let listContainer = document.getElementById('todo-list')
+    let listItem = document.createElement('li')
+    listItem.innerHTML = `
+    <button name ="checkButton"><i class = "fas fa-check-square"></i></button>
+    <span class ="todo-items">${todo}</span>
+    <button name = "deleteButton"><i class ="fas fa-trash"></i></button>
     `;
-
-    li.classList.add('todo-items');
-    ul.appendChild(li);
-    
-
+    listContainer.appendChild(listItem)
 }
 
-function checkTodo(e){
+function checkTodo(e) {
 
-    let item = e.target.parentNode;
-   
+    let item = e.target.parentNode
+
 }
